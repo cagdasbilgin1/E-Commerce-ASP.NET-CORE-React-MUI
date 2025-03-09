93cdb35e0d938546a526c8f5e86b8b76c9f76278
Cagdas Bilgin <>
2025-03-09 23:39:51 +0300

    server-error page

--- Client/src/api/request.ts
+++ Client/src/api/request.ts
@@ -1,5 +1,6 @@
 import axios, {AxiosError, AxiosResponse } from 'axios';
 import { toast } from 'react-toastify';
+import { router } from '../router/Routes';
 
 axios.defaults.baseURL = 'http://localhost:5261/api/';
 
@@ -18,7 +19,7 @@ axios.interceptors.response.use(response => {
             toast.error(data.title);
             break;
         case 500:
-            toast.error(data.title);
+            router.navigate("/server-error", {state : {error : data, status : status}});
             break;
         default:
             break;
--- Client/src/components/Header.tsx
+++ Client/src/components/Header.tsx
@@ -7,6 +7,7 @@ const links = [
     {title: "About", to: "/about"},
     {title: "Contact", to: "/contact"},
     {title: "Catalog", to: "/catalog"},
+    {title: "Error", to: "/error"},
 ]
 
 const navStyles = {
