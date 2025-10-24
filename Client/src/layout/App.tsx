82c9a1f1fd971aa1aaea224459fa9772117cac65
cagdasbilgin <cagdasbilgin1@gmail.com>
2025-10-24 22:58:46 +0300

    fix: improve auth middleware and error handling

--- Client/src/layout/App.tsx
+++ Client/src/layout/App.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import Header from "./Header";
 import {CircularProgress, Container, CssBaseline } from "@mui/material";
 import { Outlet } from "react-router";
 import { ToastContainer } from "react-toastify";
@@ -7,7 +6,8 @@ import 'react-toastify/dist/ReactToastify.css';
 import requests from "../api/requests";
 import { useAppDispatch } from "../hooks/hooks";
 import { setCart } from "../features/cart/cartSlice";
-import { setUser } from "../features/account/accountSlice";
+import { logout, setUser } from "../features/account/accountSlice";
+import Header from "./Header";
 
 function App() {
 
@@ -20,14 +20,18 @@ function App() {
         requests.Account.getUser()
             .then(user => {
                 setUser(user);
-                localStorage.setItem("user", user);
+                localStorage.setItem("user", JSON.stringify(user));
             })
-            .catch(error => console.log(error));
+            .catch(error => {
+                console.log(error);
+                dispatch(logout());
+            });
 
         requests.Cart.get()
             .then(cart => dispatch(setCart(cart)))
             .catch(error => console.log(error))
             .finally(() => setLoading(false));
+
     }, []);
 
     if (loading) return <CircularProgress />;
